e3a835967e7f5708912e20aab444eee6491c108b
Nonjabulo Chonco <nonjabulo918@gmail.com>
2025-06-09 08:29:54 -0700

    updated css

--- src/aura.js
+++ src/aura.js
@@ -1,14 +1,2 @@
 
-function temperatureSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector(".search-input");
-    let header = document.querySelector(".heading");
-    header = searchInput.value;
-    
-}
-
-
-
-
-let searchForm = document.querySelector(".search-form");
-searchForm.addEventListener("Submit", temperatureSubmit);
+alert("Hello Sena")
