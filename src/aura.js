c61db5ed66dc5c1a795570e37105948bc6a8df96
Nonjabulo Chonco <nonjabulo918@gmail.com>
2025-06-09 09:17:22 -0700

    added an id

--- src/aura.js
+++ src/aura.js
@@ -1,2 +1,15 @@
 
-alert("Hello Sena")
+function SubmitEvent(event) {
+    event.preventDefault();
+    let placeHolder = document.querySelector("#placeholder-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= placeHolder.value;
+    
+    
+}
+
+
+
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", SubmitEvent);
