32e2fa1bc7f57955df8a4269776db016b0edfdbf
Nonjabulo Chonco <nonjabulo918@gmail.com>
2025-06-11 05:30:17 -0700

    added axios

--- src/aura.js
+++ src/aura.js
@@ -1,8 +1,32 @@
+function reloadWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    
+    
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+
+     temperatureElement.innerHTML = Math.round(temperature);
+    
+}
+
+
+function searchCity(city) {
+    let apiKey = "4df1bdctd7b970a1c5dof432b8f97964";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(reloadWeather);
+    
+}
+
+
+
+
 function signUp(event) {
   event.preventDefault();
   let input = document.querySelector("#text-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = input.value;
+  
+  searchCity(input.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", signUp);
