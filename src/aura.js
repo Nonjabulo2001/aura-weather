3736205a0c04733165659955430df6d683295aba
Nonjabulo Chonco <nonjabulo918@gmail.com>
2025-06-11 07:03:01 -0700

    injected more weather data

--- src/aura.js
+++ src/aura.js
@@ -1,13 +1,37 @@
 function reloadWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time")
+    let date = new Date(response.data.time*100);
+    console.log(response.data)
     
     
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
-
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML =`${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formDate(date);
      temperatureElement.innerHTML = Math.round(temperature);
     
+}
+function formDate(date) {
+  
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  if(minutes<10){
+    minutes = `0${minutes}`
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+  
 }
 
 
